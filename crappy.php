c555b492b50b1007737b457f539d3430d57dcfef
Bego Mario Garde <pixolin@pixolin.de>
2018-08-23 18:05:32 +0200

    initial commit

---
+++ crappy.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Crappy
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adds error to test debugging. USE AT OWN RISC AND DON'T USE ON PRODUCTIVE SITES
+ * Author:          Bego Mario Garde <pixolin@pixolin.de
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     crappy
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Crappy
+ */
+
+// Your code starts here.
